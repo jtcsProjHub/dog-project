c503122684bb61c8f8b5bb1d299c2364ef853c1b
John Cameron <john.cameron@iamrobotics.com>
2018-03-17 15:32:37 -0400

    Completed Project

---
+++ dog_project.py
@@ -0,0 +1,162 @@
+from sklearn.datasets import load_files       
+from keras.utils import np_utils
+import numpy as np
+from glob import glob
+
+# define function to load train, test, and validation datasets
+def load_dataset(path):
+    data = load_files(path)
+    dog_files = np.array(data['filenames'])
+    dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
+    return dog_files, dog_targets
+
+# load train, test, and validation datasets
+train_files, train_targets = load_dataset('dogImages/train')
+valid_files, valid_targets = load_dataset('dogImages/valid')
+test_files, test_targets = load_dataset('dogImages/test')
+
+# load list of dog names
+dog_names = [item[20:-1] for item in sorted(glob("dogImages/train/*/"))]
+
+# print statistics about the dataset
+print('There are %d total dog categories.' % len(dog_names))
+print('There are %s total dog images.\n' % len(np.hstack([train_files, valid_files, test_files])))
+print('There are %d training dog images.' % len(train_files))
+print('There are %d validation dog images.' % len(valid_files))
+print('There are %d test dog images.'% len(test_files))
+
+import random
+random.seed(8675309)
+
+# load filenames in shuffled human dataset
+human_files = np.array(glob("lfw/*/*"))
+random.shuffle(human_files)
+
+# print statistics about the dataset
+print('There are %d total human images.' % len(human_files))
+
+import cv2                
+import matplotlib.pyplot as plt
+
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+# load color (BGR) image
+img = cv2.imread(human_files[3])
+# convert BGR image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# find faces in image
+faces = face_cascade.detectMultiScale(gray)
+
+# print number of faces detected in the image
+print('Number of faces detected:', len(faces))
+
+# get bounding box for each detected face
+for (x,y,w,h) in faces:
+    # add bounding box to color image
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    
+# convert BGR image to RGB for plotting
+cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# display the image, along with bounding box
+plt.imshow(cv_rgb)
+plt.show()                                                      
+
+# returns "True" if face is detected in image stored at img_path
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+human_files_short = human_files[:100]
+dog_files_short = train_files[:100]
+# Do NOT modify the code above this line.
+
+## on the images in human_files_short and dog_files_short.
+humansThatAreHuman = 0
+doggosThatAreHuman = 0
+for fileIndex in range(0, len(human_files_short)):
+    humansThatAreHuman += int(face_detector(human_files_short[fileIndex]))
+    doggosThatAreHuman += int(face_detector(dog_files_short[fileIndex]))
+    
+print("Percent of first 100 humans that appear human: " + str(humansThatAreHuman) + "%")
+print("Percent of first 100 dogs that might actually be human: " + str(doggosThatAreHuman) + "%")
+
+## (Optional) TODO: Report the performance of another  
+## face detection algorithm on the LFW dataset
+### Feel free to use as many code cells as needed.
+
+from keras.applications.resnet50 import ResNet50
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
+
+from keras.preprocessing import image                  
+from tqdm import tqdm
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+### returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151)) 
+
+### on the images in human_files_short and dog_files_short.
+humansThatAreDogs = 0
+doggosThatAreNotHumans = 0
+for fileIndex in range(0, len(human_files_short)):
+    humansThatAreDogs += int(dog_detector(human_files_short[fileIndex]))
+    doggosThatAreNotHumans += int(dog_detector(dog_files_short[fileIndex]))
+    
+print("Percent of first 100 humans that appear to be dogs instead: " + str(humansThatAreDogs) + "%")
+print("Percent of first 100 dogs that are indeed dogs: " + str(doggosThatAreNotHumans) + "%")
+
+from PIL import ImageFile                            
+ImageFile.LOAD_TRUNCATED_IMAGES = True                 
+
+# pre-process the data for Keras
+train_tensors = paths_to_tensor(train_files).astype('float32')/255
+valid_tensors = paths_to_tensor(valid_files).astype('float32')/255
+test_tensors = paths_to_tensor(test_files).astype('float32')/255
+
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
+
+model = Sequential()
+
+### TODO: Define your architecture.
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', 
+                        input_shape=(32, 32, 3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
